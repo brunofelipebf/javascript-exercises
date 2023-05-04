25b916fb8a79fd6e3de511f870b40fae9a311d4d
Bruno <bfsilva98@gmail.com>
2023-05-04 12:30:55 -0300

    update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
 
+let frase = "Olá"
+let vezes = 3
+const repeatString = function(frase,vezes) {
+    for(i=0; i < vezes; i++){
+        return frase;
+    }
 };
-
+/*This function will take two arguments, `string` and `num`.*/
 // Do not edit below this line
 module.exports = repeatString;
